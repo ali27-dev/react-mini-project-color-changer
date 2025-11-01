927b1fa21373a7245d0bacee8fb2ac9baee0964d
ali27-dev <khanali3229@gmail.com>
2025-11-01 22:49:41 +0500

    The logic of backgroundColor changing is implemented and its working fine.The commented code is my own try to implement the functionality but its falied.

--- src/NavbarPannel.jsx
+++ src/NavbarPannel.jsx
@@ -2,52 +2,68 @@ import React, { useState } from "react";
 import "./css/NavbarPannel.css";
 
 function NavbarPannel() {
-  let [intColor, setIntColor] = useState(0);
+  let [intColor, setIntColor] = useState("black");
 
-  const btnHandler = () => {
-    if (intColor === ".Red") {
-      let backgroundColor = (intColor.style.backgroundColor = "red");
-      setIntColor(backgroundColor);
-    } else if (intColor === ".Green") {
-      let backgroundColor = (intColor.style.backgroundColor = "Green");
-      setIntColor(backgroundColor);
-    }
-  };
+  // const btnHandler = () => {
+  //   if (intColor == "black") {
+  //     setIntColor("red");
+  //   } else if (intColor == "red") {
+  //     setIntColor("green");
+  //   } else if (intColor == "green") {
+  //     setIntColor("blue");
+  //   } else if (intColor == "blue") {
+  //     setIntColor("olive");
+  //   } else if (intColor == "olive") {
+  //     setIntColor("gray");
+  //   } else if (intColor == "gray") {
+  //     setIntColor("yellow");
+  //   } else if (intColor == "yellow") {
+  //     setIntColor("pink");
+  //   } else if (intColor == "pink") {
+  //     setIntColor("purple");
+  //   } else if (intColor == "purple") {
+  //     setIntColor("lavender");
+  //   } else if (intColor == "lavender") {
+  //     setIntColor("white");
+  //   } else if (intColor == "white") {
+  //     setIntColor("black");
+  //   }
+  // };
   return (
-    <div className="container">
+    <div className="container" style={{ backgroundColor: intColor }}>
       <nav className="navbar-pannel">
         <div className="btn-box">
-          <button className="Red" onClick={btnHandler}>
+          <button className="Red" onClick={() => setIntColor("red")}>
             Red
           </button>
-          <button className="Green" onClick={btnHandler}>
+          <button className="Green" onClick={() => setIntColor("green")}>
             Green
           </button>
-          <button className="Blue" onClick={btnHandler}>
+          <button className="Blue" onClick={() => setIntColor("blue")}>
             Blue
           </button>
-          <button className="Olive" onClick={btnHandler}>
+          <button className="Olive" onClick={() => setIntColor("olive")}>
             Olive
           </button>
-          <button className="Gray" onClick={btnHandler}>
+          <button className="Gray" onClick={() => setIntColor("gray")}>
             Gray
           </button>
-          <button className="Yellow" onClick={btnHandler}>
+          <button className="Yellow" onClick={() => setIntColor("yellow")}>
             Yellow
           </button>
-          <button className="Pink" onClick={btnHandler}>
+          <button className="Pink" onClick={() => setIntColor("pink")}>
             Pink
           </button>
-          <button className="Purple" onClick={btnHandler}>
+          <button className="Purple" onClick={() => setIntColor("purple")}>
             Purple
           </button>
-          <button className="Lavender" onClick={btnHandler}>
+          <button className="Lavender" onClick={() => setIntColor("lavender")}>
             Lavender
           </button>
-          <button className="White" onClick={btnHandler}>
+          <button className="White" onClick={() => setIntColor("white")}>
             White
           </button>
-          <button className="Black" onClick={btnHandler}>
+          <button className="Black" onClick={() => setIntColor("black")}>
             Black
           </button>
         </div>
